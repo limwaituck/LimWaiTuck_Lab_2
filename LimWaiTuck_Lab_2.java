97f1c2353ad25ed94e2b912cd2c0e6886082c33c
limwaituck <147128317+limwaituck@users.noreply.github.com>
2023-10-09 23:16:02 +0800

    Add files via upload

---
+++ LimWaiTuck_Lab_2.java
@@ -0,0 +1,30 @@
+//Name: Lim Wai Tuck
+//Tutorial Group: T05
+//Java version: 1.8
+//Name of the program: LimWaiTuck_Lab_1.java
+//Objective: Lab 2
+//Declaration: This is my own lab. I have not passed my lab to anyone else in this class.
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
